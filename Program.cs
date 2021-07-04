374b19c4cacf427d0c749216e44993e1998aeb93
Aisling McGettigan <39455192+AislingMcGettigan@users.noreply.github.com>
2021-07-04 20:15:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            var num = random.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 0 and 100");
+                string input = Console.ReadLine();
+                var i = int.Parse(input);
+
+                if (i > num)
+                {
+                    Console.WriteLine("Number is too high, try again");
+                }
+                if (i < num)
+                {
+                    Console.WriteLine("Number is too low, try again");
+                }
+
+                if (i == num)
+                {
+                    Console.WriteLine("You win!");
+                    win = true;
+                }
+            }
+            while (win == false);
+        }
+    }
+}
